a5e7fccd772efb13e4fa147fe916fee86b66f6de
ulkg <ulrich.gram@protonmail.com>
2024-06-26 22:09:05 +0200

    Add comments user serializers

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -6,15 +6,20 @@ from rest_framework import serializers
 # and then convert them to native python data types that can be rendered into JSON / content types.
 # Also able to perform deserialization
 class UserSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(read_only=True)
+    
+    # write_only - don't show password hashes with required false
     password = serializers.CharField(write_only=True, required=False)
-
-    def created(self, validated_data):
+    
+    # called when post request is made
+    def create(self, validated_data): 
+        # pop returns value of key and password key is removed from validated data
         password = validated_data.pop('password')
+        # create new user object in database using validated data fields
         user = User.objects.create(**validated_data)
+        # take care of hashing logic
         user.set_password(password)
+        # perform db transaction
         user.save()
-
         return user
     
     class Meta:
